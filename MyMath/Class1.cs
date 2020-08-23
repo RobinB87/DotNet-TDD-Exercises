66aa5aec025936f0035279f26957c102d67e98be
Bette, Robin <robin.bette@capgemini.com>
2020-08-23 15:19:17 +0200

    fix

--- MyMath/Class1.cs
+++ MyMath/Class1.cs
@@ -15,12 +15,12 @@ namespace MyMath
                 throw new ArgumentOutOfRangeException();
             }
 
-            double result = input;
-            double previousResult = -input;
+            var result = input;
+            var previousResult = -input;
             while (Math.Abs(previousResult - result) > result / 1000)
             {
                 previousResult = result;
-                result -= (result * result - input) / (2 * result);
+                result = (result + input / result) / 2;
             }
             return result;
         }
